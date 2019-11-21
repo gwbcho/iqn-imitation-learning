0941936b4ccc8a57069783815ca9dc6016a827fe
gwbcho <gregwbcho@gmail.com>
2019-11-21 01:07:53 -0500

    IL-000 Initial setup.

--- IDP/agent.py
+++ IDP/agent.py
@@ -3,20 +3,20 @@ import numpy as np
 import tensorflow as tf
 
 # import local dependencies
-from GAC.networks import StochasticActor, AutoRegressiveStochasticActor,  Critic, Value
-from GAC.helpers import ReplayBuffer, update, ActionSampler
+from IDP.networks import StochasticActor, AutoRegressiveStochasticActor,  Critic, Value
+from IDP.helpers import ReplayBuffer, update, ActionSampler
 
 
 """
 File Description:
 
-Class: Generative Actor Critic (GAC) agent.
+Class: Immitation Distributional Policy (IDP) agent.
 """
 
 
-class GACAgent:
+class IDPAgent:
     """
-    GAC agent.
+    IDP agent.
     Action is alway from -1 to 1 in each dimension.
     Will not do normalization.
     """
@@ -34,24 +34,8 @@ class GACAgent:
         self.state_dim = args.state_dim
         if args.actor == 'IQN':
             self.actor = StochasticActor(args.state_dim, args.action_dim)
-            self.target_actor = StochasticActor(args.state_dim, args.action_dim)
         elif args.actor == 'AIQN':
             self.actor = AutoRegressiveStochasticActor(args.state_dim, args.action_dim)
-            self.target_actor = AutoRegressiveStochasticActor(args.state_dim, args.action_dim)
-
-        self.critics = Critic(args.state_dim, args.action_dim)
-        self.target_critics = Critic(args.state_dim, args.action_dim)
-
-        self.value = Value(args.state_dim)
-        self.target_value = Value(args.state_dim)
-
-        # initialize the target networks.
-        update(self.target_actor, self.actor, 1.0)
-        update(self.target_critics.model1, self.critics.model1, 1.0)
-        update(self.target_critics.model2, self.critics.model2, 1.0)
-        update(self.target_value.model, self.value.model, 1.0)
-
-        self.replay = ReplayBuffer(args.state_dim, args.action_dim, args.buffer_size)
         self.action_sampler = ActionSampler(self.actor.action_dim)
 
     def train_one_step(self):
@@ -68,14 +52,6 @@ class GACAgent:
         # transitions is sampled from replay buffer
         transitions = self.replay.sample_batch(self.args.batch_size)
         # transitions is sampled from replay buffer
-        critic_history = self.critics.train(transitions, self.target_value, self.args.gamma)
-        value_history = self.value.train(
-            transitions,
-            # self.action_sampler # WE WILL DECIDE WHETHER WE NEED THIS LATER
-            self.target_actor,
-            self.target_critics,
-            self.args.action_samples
-        )
         states, actions, advantages = self._sample_positive_advantage_actions(transitions.s)
         if advantages.shape[0]:
             self.actor.train(
@@ -85,20 +61,13 @@ class GACAgent:
                 self.args.mode,
                 self.args.beta
             )
-
         update(self.target_actor, self.actor, self.args.tau)
-        update(self.target_critics.model1, self.critics.model1, self.args.tau)
-        update(self.target_critics.model2, self.critics.model2, self.args.tau)
-        update(self.target_value.model, self.value.model, self.args.tau)
-
         return critic_history, value_history
 
     def _sample_positive_advantage_actions(self, states):
         """
-        Sample from the target network and a uniform distribution.
-        Then only keep the actions with positive advantage.
-        Returning one action per state, if more needed, make states contain the
-        same state multiple times.
+        Sample all more advantageous actions dtermined by the expert given the agent's current state
+
 
         Args:
             states (tf.Variable): dimension (batch_size * K, state_dim)
@@ -141,17 +110,3 @@ class GACAgent:
             sampled actions for the given state with dimension (batch_size, action_dim)
         """
         return self.action_sampler.get_actions(self.actor, states)
-
-    def store_transitions(self, state, action, reward, next_state, is_done):
-        """
-        Store the transition in the replay buffer.
-
-        Args:
-            # TODO
-            state
-            action
-            reward
-            next_state
-            is_done
-        """
-        self.replay.store(state, action, reward, next_state, is_done)
